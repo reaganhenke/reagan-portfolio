d2044bfde149885aa93210bd1acd6e337520c61a
Reagan Henke <reaganhenke@gmail.com>
2024-01-28 12:55:41 -0500

    remove environments

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,8 +3,7 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAnalytics, provideAnalytics, ScreenTrackingService } from '@angular/fire/analytics';
-import { firebaseConfig } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp(firebaseConfig))), importProvidersFrom(provideAnalytics(() => getAnalytics())), ScreenTrackingService]
+  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({ "projectId": "reaganportfolio-312cf", "appId": "1:97976991800:web:9c37946f4efdd7b104a6cd", "storageBucket": "reaganportfolio-312cf.appspot.com", "apiKey": "AIzaSyCEVjHLD6IpQ4QYjjpGOd45nAD5-TofrxY", "authDomain": "reaganportfolio-312cf.firebaseapp.com", "messagingSenderId": "97976991800", "measurementId": "G-WM6QSB3CX6" }))), importProvidersFrom(provideAnalytics(() => getAnalytics())), ScreenTrackingService]
 };
